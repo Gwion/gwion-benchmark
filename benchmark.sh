b5763698153a250a92b8489d8d5a117d05ea2ecd
Jérémie Astor <fennecdjay@gmail.com>
2021-02-14 18:09:13 +0100

    :art: Add horse64

--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-language=("gwion" "wren" "lua" "python" "chuck" "ruby")
-extension=("gw" "wren" "lua" "py" "ck" "rb")
+language=("gwion" "wren" "lua" "python" "chuck" "ruby" "horsec")
+extension=("gw" "wren" "lua" "py" "ck" "rb" "h64")
 test_dir="src"
 result_dir="results"
 plot_script="bench.plot"
