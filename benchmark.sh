39c52309a84254eb1c700fc8a71f425f89867bf0
Jérémie Astor <fennecdjay@gmail.com>
2021-02-14 22:13:24 +0100

    :art: Call a horse a horse64

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-language=("gwion" "wren" "lua" "python" "chuck" "ruby" "horse")
+language=("gwion" "wren" "lua" "python" "chuck" "ruby" "horse64")
 extension=("gw" "wren" "lua" "py" "ck" "rb" "h64")
 test_dir="src"
 result_dir="results"
