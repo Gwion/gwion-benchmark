786197e1b245ae5378eb7c5c0464b3334a09db66
Jérémie Astor <fennecdjay@gmail.com>
2021-02-19 00:15:26 +0100

    Improve versions

--- version.sh
+++ version.sh
@@ -3,9 +3,9 @@
 cat << EOF
 gwion: latest
 wren: $(wren --version)
-lua: $(lua -v | cut -d" " -f1,2)
-python: $(python --version)
+lua: $(lua -v | cut -d" " -f2)
+python: $(python --version [ cut -d" " -f2)
 chuck: $(chuck --version 2>&1 | head -2 | tail -1 | cut -d " " -f 3-5)
-ruby: $(ruby -v | cut -d" " -f1,2)
+ruby: $(ruby -v | cut -d" " -f2)
 horse64: $(horse64 --short-version)
 EOF
